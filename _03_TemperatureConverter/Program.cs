3ceae9fce640e5f24949fd9c42f788a9764c3cb2
Mobin-mahmoudzadeh <mobin.mahmoudzadehhh@gmail.com>
2026-06-16 19:55:13 +0330

    feat : add Display Welcome Message & Get input in project

---
+++ _03_TemperatureConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03_TemperatureConverter
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // --- STEP 1: Display Welcome Message & Get Input ---
+            Console.WriteLine("=== Temperature Converter (Celsius to Fahrenheit) ===");
+            Console.WriteLine("-----------------------------------------------------");
+            
+            Console.Write("Enter temperature in Celsius (e.g., 25.5): ");
+            
+            
+        }
+        
+    }
+};
+
