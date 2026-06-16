3422f03eb0fa8b37dc182ebe9f0c062dab5725bf
Mobin-mahmoudzadeh <mobin.mahmoudzadehhh@gmail.com>
2026-06-16 08:38:29 +0330

    feat: add student name and 5 subject grade inputs

--- _01_StudentGradeCalculator/Program.cs
+++ _01_StudentGradeCalculator/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+
+namespace _01_StudentGradeCalculator
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // --- STEP 1: Welcome Message & Inputs ---
+            Console.WriteLine("=== Student Grade Calculator ===");
+            Console.WriteLine("Please enter the student details below:\n");
+            
+            //Get Student Name 
+            Console.Write("Enter Student Name: ");
+            string studentName = Console.ReadLine();
+            
+            // Get Grades for 5 subjects
+            Console.Write("Enter Math grade: ");
+            double mathGrade = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Physics grade: ");
+            double physicsGrade = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Chemistry garde: ");
+            double chemistryGrade = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Biology garde: ");
+            double biologyGrade = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Geography grade: ");
+            double geographyGrade = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
