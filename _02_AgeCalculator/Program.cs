fd752836b1c026884805db9b692f19f258a6240a
Mobin-mahmoudzadeh <mobin.mahmoudzadehhh@gmail.com>
2026-06-16 19:11:15 +0330

    feat: calculate age and display formatted result

--- _02_AgeCalculator/Program.cs
+++ _02_AgeCalculator/Program.cs
@@ -10,11 +10,24 @@ namespace _02_AgeCalculator
             Console.WriteLine("=== Age Calculator ===");
             
             // Defining the current year as a constant
-            const int currentYear = 2026;
+            const int CurrentYear = 2026;
             
             // Prompting user for their birth year
             Console.Write("Enter your birth year (e.g.2005)");
             int birthYear = Convert.ToInt32(Console.ReadLine());
+            
+            // --- STEP 2: Calculate Age & Display Output ---
+            // Calculating the age
+            int age = CurrentYear - birthYear;
+
+            // Displaying the result using string interpolation
+            Console.WriteLine("\n---------------------------------");
+            Console.WriteLine($"In the year {CurrentYear}, you are {age} years old.");
+            Console.WriteLine("---------------------------------");
+
+            // Keep the console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
