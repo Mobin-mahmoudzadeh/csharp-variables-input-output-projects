be2783825e4a68ca723fd661a54c346283d8ea3c
Mobin-mahmoudzadeh <mobin.mahmoudzadehhh@gmail.com>
2026-06-18 11:28:03 +0330

    feat: implement area calculation logic using width and length inputs

--- _05_RectangleCalculator/Program.cs
+++ _05_RectangleCalculator/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+namespace _05_RectangleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Rectangle Area Calculator ===");
+            Console.WriteLine("---------------------------------");
+            
+            // --- STEP 1: Get Inputs ---
+            Console.Write("Enter the width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter the length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            
+            // --- STEP 2: Calculation ---
+            double area =  width * length;
+            
+            // --- STEP 3: Display Result ---
+            Console.WriteLine("\\n------------------- Result -------------------");
+            Console.WriteLine($"width: {width:F2}, Length: {length:F2}");
+            Console.WriteLine($"The area of the rectangle is: {area:F2}");
+            Console.WriteLine("----------------------------------------------");
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
